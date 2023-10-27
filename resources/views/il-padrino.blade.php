46599a1eabfb734f2b0c3c5e78ad9cf911f88d1c
vgianluca96 <gianluca.vallese@hotmail.com>
2023-10-27 16:39:27 +0200

    modifiche pag il padrino pt 2

--- resources/views/il-padrino.blade.php
+++ resources/views/il-padrino.blade.php
@@ -14,16 +14,16 @@
     </div>
     <div class="w-50">
         <ul class="list-group">
-            <li class="list-group-item">
+            <li class="list-group-item list-group-item-dark">
                 <strong>Titolo originale: </strong>{{$movies[0]['original_title']}}
             </li>
-            <li class="list-group-item">
+            <li class="list-group-item list-group-item-light">
                 <strong>Nazionalità: </strong>{{$movies[0]['nationality']}}
             </li>
-            <li class="list-group-item">
+            <li class="list-group-item list-group-item-dark">
                 <strong>Data uscita: </strong>{{$movies[0]['date']}}
             </li>
-            <li class="list-group-item">
+            <li class="list-group-item list-group-item-light">
                 <strong>Voto: </strong>{{$movies[0]['vote']}}
             </li>
           </ul>
